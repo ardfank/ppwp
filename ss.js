72186a7123e097e8a32b140b2cbb2397eb62722f
asd <you@sa.ya>
2024-02-19 20:14:05 +0800

    asd

--- ss.js
+++ ss.js
@@ -1,4 +1,5 @@
-prov=JSON.parse('{"1":"total","11":"ACEH","51":"BALI","36":"BANTEN","17":"BENGKULU","34":"DAERAH ISTIMEWA YOGYAKARTA","31":"DKI JAKARTA","75":"GORONTALO","15":"JAMBI","32":"JAWA BARAT","33":"JAWA TENGAH","35":"JAWA TIMUR","61":"KALIMANTAN BARAT","63":"KALIMANTAN SELATAN","62":"KALIMANTAN TENGAH","64":"KALIMANTAN TIMUR","65":"KALIMANTAN UTARA","19":"KEPULAUAN BANGKA BELITUNG","21":"KEPULAUAN RIAU","18":"LAMPUNG","99":"Luar Negeri","81":"MALUKU","82":"MALUKU UTARA","52":"NUSA TENGGARA BARAT","53":"NUSA TENGGARA TIMUR","91":"P A P U A","92":"PAPUA BARAT","96":"PAPUA BARAT DAYA","95":"PAPUA PEGUNUNGAN","93":"PAPUA SELATAN","94":"PAPUA TENGAH","14":"RIAU","76":"SULAWESI BARAT","73":"SULAWESI SELATAN","72":"SULAWESI TENGAH","74":"SULAWESI TENGGARA","71":"SULAWESI UTARA","13":"SUMATERA BARAT","16":"SUMATERA SELATAN","12":"SUMATERA UTARA"}');
+var q=new URLSearchParams(window.location.search).get("q");
+var cq=(q!=null || q!=undefined)?q:'ppwp';
 function toggleDataSeries(e) {
 	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
 		e.dataSeries.visible = false;
@@ -9,10 +10,10 @@ function toggleDataSeries(e) {
 	e.chart.render();
 }
 var kabl;
-$.ajax({url:"https://ppwp.networkreverse.com/ppwp.json",dataType:"json",success:function(res){
-	kabl=res;
-	$.each(prov,function(a,b){
-		var cd = "<option value='"+b+"'>"+b+"</option>";
+$.ajax({url:"https://ppwp.networkreverse.com/"+cq+".json",dataType:"json",success:function(res){
+	kabl=res;console.log(Object.keys(res)[0]);
+	$.each(kabl[Object.keys(res)[0]],function(a,b){
+		var cd = "<option value='"+a+"'>"+a+"</option>";
 		$("#kabss").append(cd);
 	});
 }}).done(function(){
@@ -118,7 +119,7 @@ function tt(k){
 function rr(){
 	var ab = $('#kabss').val();
 	var tn = Date.now();
-	$.ajax({url:"https://ppwp.networkreverse.com/ppwp.json",dataType:"json",success:function(res){
+	$.ajax({url:"https://ppwp.networkreverse.com/"+cq+".json",dataType:"json",success:function(res){
 			var ls = Object.keys(res).pop();
 			var kls = Object.keys(kabl).pop();
 			var amin=parseInt(res[ls][ab][0]);
