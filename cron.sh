a7b86af6942f839902f00ea1771000b9433a1d03
asd <you@sa.ya>
2024-02-22 22:19:30 +0800

    asdf

--- cron.sh
+++ cron.sh
@@ -5,14 +5,19 @@ m=$(date +"%M")
 if [ $m -lt 5 ]; then
 echo "===$(date) Start ===" > /mnt/log/game/ppwp/cron.log
 fi
-sleep 5
+sleep 1
 echo "===$(date) 1st ===" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
 /usr/bin/git -C /mnt/log/game/ppwp/ commit -m "Auto `date "+ %A, %d-%m-%Y %H.%M.%S"`"
 /usr/bin/git -C /mnt/log/game/ppwp/ push
-sleep 40
 echo "===$(date) 2nd ===" >> /mnt/log/game/ppwp/cron.log
+/usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
+/usr/bin/git -C /mnt/log/game/ppwp/ add .
+/usr/bin/git -C /mnt/log/game/ppwp/ commit -m "Auto `date "+ %A, %d-%m-%Y %H.%M.%S"`"
+/usr/bin/git -C /mnt/log/game/ppwp/ push
+sleep 10
+echo "===$(date) 3rdd ===" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 echo "===$(date) END ===\n\n" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
