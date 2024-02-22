24489ee81b420bcbc42de25b69a978dbb4cd68ab
asd <you@sa.ya>
2024-02-22 12:06:24 +0800

    cron

--- cron.sh
+++ cron.sh
@@ -1,8 +1,11 @@
- 
 #!/bin/bash
+m=$(date +"%M")
+if [ $m -lt 5 ]; then
+echo "===$(date) Start ===" > /mnt/log/game/ppwp/cron.log
+fi
 /usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
 sleep 1
-/usr/bin/php /mnt/log/game/ppwp/json/prov.php > /mnt/log/game/ppwp/cron.log
+/usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
 /usr/bin/git -C /mnt/log/game/ppwp/ commit -m "Auto `date "+ %A, %d-%m-%Y %H.%M.%S"`"
 /usr/bin/git -C /mnt/log/game/ppwp/ push
