6118d69ded0643b5a55693cba03ba4b2c3a6c988
asd <you@sa.ya>
2024-02-22 15:09:57 +0800

    cronlll

--- cron.sh
+++ cron.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+/usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
 m=$(date +"%M")
 if [ $m -lt 5 ]; then
 echo "===$(date) Start ===" > /mnt/log/game/ppwp/cron.log
 fi
-/usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
 sleep 5
 echo "===$(date) 1st ===" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
 /usr/bin/git -C /mnt/log/game/ppwp/ commit -m "Auto `date "+ %A, %d-%m-%Y %H.%M.%S"`"
 /usr/bin/git -C /mnt/log/game/ppwp/ push
-sleep 5
-/usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
 sleep 5
 echo "===$(date) 2nd ===" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
