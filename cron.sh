07ac1b9d3c5a6c66ad887247ad5404a892464596
asd <you@sa.ya>
2024-02-22 12:25:23 +0800

    cron

--- cron.sh
+++ cron.sh
@@ -4,7 +4,7 @@ if [ $m -lt 5 ]; then
 echo "===$(date) Start ===\n" > /mnt/log/game/ppwp/cron.log
 fi
 /usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
-sleep 30
+sleep 5
 echo "===$(date) 1st ===\n" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
@@ -12,7 +12,7 @@ echo "===$(date) 1st ===\n" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ push
 sleep 5
 /usr/bin/git -C /mnt/log/game/ppwp/ pull origin main
-sleep 1
+sleep 5
 echo "===$(date) 2nd ===\n" >> /mnt/log/game/ppwp/cron.log
 /usr/bin/php /mnt/log/game/ppwp/json/prov.php >> /mnt/log/game/ppwp/cron.log
 /usr/bin/git -C /mnt/log/game/ppwp/ add .
