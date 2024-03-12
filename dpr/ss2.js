fa13d88a997f8ad67b39e7a6c749e699877eaa1b
asd <you@sa.ya>
2024-03-12 20:14:22 +0800

    sdasd

--- dpr/ss2.js
+++ dpr/ss2.js
@@ -165,7 +165,7 @@ function oo(e){
 	ls=(kabl[ls] !== undefined && kabl[ls] !== null )?ls:Object.keys(kabl).pop();
 	$("#pos .isin").html("");
 	var ab = $('#kabss').val();
-	var ba = (cq=='ppwp')?'':$('#kab').val();
+	var ba = (cq=='ppwp')?'TOTAL':$('#kab').val();
 	$.each(kabl[ls],function(a,b){
 		var bx = (Object.values(b).reduce((acc, o) => acc + o, 0)-b.persen);
 		var pm=Math.max(...Object.values(b));
@@ -176,7 +176,8 @@ function oo(e){
 				var qa=(b[c]==pm)?" pq":"";
 				th+="<th>"+d+"<br/><span class='p"+qa+"'>"+pa+"%</span><br/>"+b[c].toLocaleString('id')+"</th>";
 			});
-			$("#pos thead").html("<tr><th>"+ba.toUpperCase()+"<br/><span class='p'>TOTAL</span><br/>(<i>"+new Date(parseInt(ls)).toLocaleString('nl-NL')+"</i>)</th>"+th+"</tr>");
+			// th+="<th>PROGRESS<br/><span class='p'>"+b.persen+"%</span><br/>"+bx.toLocaleString('id')+"</th>";
+			$("#pos thead").html("<tr><th>"+ba.toUpperCase()+"<br/><span class='p'>"+b.persen+"%</span><br/>"+bx.toLocaleString('id')+"<br/>(<i>"+new Date(parseInt(ls)).toLocaleString('nl-NL')+"</i>)</th>"+th+"</tr>");
 		}else{
 			var td;
 			$.each(prt,function(c,d){
@@ -184,7 +185,8 @@ function oo(e){
 				var qa=(b[c]==pm)?" pq":"";
 				td+="<td data-sort='"+pa+"'><span class='p"+qa+"'>"+pa+"%</span><br/>"+b[c].toLocaleString('id')+"</td>";
 			});
-			$("#pos .isin").append("<tr><td><span style='cursor:pointer' onclick='ff(\""+a+"\")'>"+a+"</span></td>"+td+"</tr>");
+			// td+="<td data-sort='"+b.persen+"'><span class='p'>"+b.persen+"%</span><br/>"+bx.toLocaleString('id')+"</td>";
+			$("#pos .isin").append("<tr><td><span class='p' style='cursor:pointer' onclick='ff(\""+a+"\")'>"+a+"</span><br/>"+b.persen+"% ("+bx.toLocaleString('id')+")</td>"+td+"</tr>");
 		}
 	});
 	ab=(ab==="total")?"":ab;
